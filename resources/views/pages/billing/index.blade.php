52daf022c43680ab3f7c8206d5126c58c8093d7a
oliverzolan <oliverzolan@gmail.com>
2024-09-04 20:29:58 +0000

    2

--- resources/views/pages/billing/index.blade.php
+++ resources/views/pages/billing/index.blade.php
@@ -25,16 +25,18 @@
             </div>
             <div class="col-auto">
                <!-- Nav -->
-               <div class="text-right">
-               <h6 class="header-pretitle text-secondary-light">
-                  {{utrans("headers.information")}}
-               </h6>
-               <p class="text-white mb-0">
-                     {{utrans("profile.name")}}
-               </p>
-               <p class="text-white mb-0">
-                     {{utrans("profile.address")}}
-               </p>
+               <ul class="nav nav-tabs header-tabs">
+                  <li class="nav-item">
+                     <a class="nav-link text-right">
+                        <h6 class="header-pretitle text-secondary-light">
+                           {{utrans("headers.amountDue")}}
+                        </h6>
+                        <h3 class="text-white mb-0">
+                           {{Formatter::currency($values['amount_due'])}}
+                        </h3>
+                     </a>
+                  </li>
+               </ul>
             </div>
          </div>
          <!-- / .row -->
